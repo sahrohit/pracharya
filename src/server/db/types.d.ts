fb04c83823a5b90c842016a65e4a028a7fd79959
Rohit Sah <sahrohit9586@gmail.com>
2024-03-05 12:44:21 +0545

    fix(app): :bug: fixed type mismatch with select issue

--- src/server/db/types.d.ts
+++ src/server/db/types.d.ts
@@ -15,7 +15,7 @@ export type SelectChapter = typeof chapters.$inferSelect;
 export type SelectSubChapter = typeof subChapters.$inferSelect;
 
 export type SelectIssueWithQuestion = SelectIssue & {
-	creator: SelectUser;
+	creator: Omit<SelectUser, "password">;
 	question:
 		| (SelectQuestion & {
 				options: SelectOption[];
